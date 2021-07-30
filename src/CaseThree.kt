40fae3e33fa942d8834e377e0f1f5b29cab9ce67
wahyurama-creator <wahyuramas32@gmail.com>
2021-07-30 17:24:19 +0700

    Complete case three

--- src/CaseThree.kt
+++ src/CaseThree.kt
@@ -1,14 +1,20 @@
-// Uncompleted !!!
-
 fun main() {
     println("Format is 12:00 PM")
     print("Input your time: ")
     val input = readLine()
-    convertTime(input.toString())
+    println(convertTime(input.toString()))
 }
 
-private fun convertTime(value: String) {
-    val beforeLast = value.substringBeforeLast(":")
-    val afterLast = value.substringAfterLast(":")
-    println("$beforeLast:$afterLast")
+private fun convertTime(input: String) : String{
+    var before = input.substringBeforeLast(":").toInt()
+    val after = input.substringAfterLast(":").dropLast(3)
+    val value = input.substringAfterLast(" ")
+
+    if (value.contains("p", true) && before < 12) {
+        before += 12
+    } else if (value.contains("a", true) && before == 12) {
+        before -= 12
+    }
+
+    return "$before:$after"
 }
